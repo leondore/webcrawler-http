b9afcb22dfc9499b32d70d381650dc1208bc69ae
Leon Dore <leon.dore@gmail.com>
2023-09-12 08:44:31 -0400

    Scaffold initial functionality for main function

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+const { argv } = require('node:process');
 
+function main() {
+  const [execPath, filePath, ...args] = argv;
+
+  if (args.length < 1) {
+    console.error('Please provide a URL');
+    process.exit(1);
+  }
+
+  if (args.length > 1) {
+    console.error(`1 argument expected ${args.length} provided`);
+    process.exit(1);
+  }
+
+  const baseUrl = args[0];
+  console.log(`Crawler starting at ${baseUrl}...`);
+}
+
+main();
